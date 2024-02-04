37e6327644217f7c7943186f31e5fa9d826cae82
Pieter Pretorius | Ixo <pieter.pretorius@ixo.world>
2024-02-04 12:59:39 +0200

    fix: fixing graphql urls

--- constants/api.ts
+++ constants/api.ts
@@ -3,18 +3,11 @@ import { ChainNetwork } from 'types/chain';
 export const BLOCKSYNC_GRAPHQL_URLS: { [network in ChainNetwork]: string } = {
   mainnet: 'https://blocksync-graphql.ixo.earth/graphql',
   testnet: 'https://testnet-blocksync-graphql.ixo.earth/graphql',
-  devnet: 'http://localhost:8080/graphql',
+  devnet: 'https://devnet-blocksync-graphql.ixo.earth/graphql',
 };
 
-// export const BLOCKSYNC_API_URLS: { [network in ChainNetwork]: string } = {
-//   mainnet: 'https://blocksync-graphql.ixo.earth/api',
-//   testnet: 'https://testnet-blocksync-graphql.ixo.earth/api',
-//   devnet: 'https://devnet-blocksync-graphql.ixo.earth/api',
-// };
-
-// TODO: go to original source once blocksync changes deloyed
 export const BLOCKSYNC_API_URLS: { [network in ChainNetwork]: string } = {
   mainnet: 'https://blocksync-graphql.ixo.earth/api',
   testnet: 'https://testnet-blocksync-graphql.ixo.earth/api',
-  devnet: 'http://localhost:8080/api',
+  devnet: 'https://devnet-blocksync-graphql.ixo.earth/api',
 };
